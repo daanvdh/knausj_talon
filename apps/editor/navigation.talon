75699368196e605c9e84b10a236747b0b712d745
Daan van den Heuvel <d.vandenheuvel@eyefreight.com>
2021-01-13 13:35:51 +0100

    add Symbol navigation

--- apps/editor/navigation.talon
+++ apps/editor/navigation.talon
@@ -4,3 +4,5 @@
 go back: user.jump_back()
 go forward: user.jump_forward()
 go inside: user.go_inside()
+before <user.symbol_key>: user.before(symbol_key)
+after <user.symbol_key>: user.after(symbol_key)
--- code/navigation.py
+++ code/navigation.py
@@ -2,6 +2,7 @@
 # These interfaces can then be implemented per editor and called directly from .talon files by 
 # prefixing the method name with "user.".
 
+import time
 from talon import ctrl, ui, Module, Context, actions, clip
 
 mod = Module()
@@ -36,4 +37,32 @@ class Actions:
         """Go to the previous method definition"""
 
     def go_next_method():
-        """Go to the next method definition"""                
+        """Go to the next method definition"""
+
+    def before(symbol: str):
+        """put the cursor before the given symbol"""
+        before(ascii(symbol))
+        
+    def after(symbol: str):
+        """put the cursor after the given symbol"""
+        after(ascii(symbol))
+                        
+def before(symbol):
+    with clip.revert():
+        actions.edit.extend_line_end()
+        time.sleep(0.1)
+        actions.edit.copy()
+        actions.edit.left()
+        time.sleep(0.1)
+        text_right = clip.get().lower()
+    i = 0
+    while i < len(text_right) - 1 and not symbol[1] == text_right[i]:
+        i += 1
+    if symbol[1] == text_right[i]:
+        for j in range(0, i):
+            actions.edit.right()
+        return True
+    return False
+
+def after(symbol):
+    if before(symbol): actions.edit.right()
